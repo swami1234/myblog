a077928ea76dd2aa5f7912a874d61973122b8b81
swami1234 <34219565+swami1234@users.noreply.github.com>
2019-11-11 14:23:41 -0500

    Initial commit

---
+++ form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Forms</title>
+    <meta charset="utf-8">
+</head>
+<body>
+
+<form method="post">
+
+<input name="username">
+<input name="password" type="password">
+
+<button>Send</button>
+</form>
+
+
+
+</body>
+</html>
